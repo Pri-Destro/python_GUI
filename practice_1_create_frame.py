09f99ed42f2817219ad8a567ed47c0beca51d6d8
z3rO1997 <zer01997@naver.com>
2020-09-21 14:56:41 +0900

    frame

---
+++ practice_1_create_frame.py
@@ -0,0 +1,9 @@
+from tkinter import*
+
+root = Tk()
+root.title("mine")
+root.geometry("640x480") # 가로 * 세로
+#root.geometry("640x480+500+200") # 가로 * 세로 + x 좌표 + y 좌표
+
+root.resizable(False, False) # 너비 높이 변경 불가
+root.mainloop()
